be4fb7baf269dedfe3973d4acf549eeeaec74e9a
Viviana Tran <tranviviana@berkeley.edu>
2024-06-04 16:41:24 -0700

    finished rock paper scissors

---
+++ JS course/rockpaperscissors/script.js
@@ -0,0 +1,60 @@
+const selectionButtons = document.querySelectorAll(`[data-selection]`)
+const finalColumn = document.querySelector(`[data-final-column]`)
+const computerScoreSpan = document.querySelector(`[data-comp-score]`)
+const yourScoreSpan = document.querySelector (`[data-your-score]`)
+
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '🪨',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '📃',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: ' ✂',
+        beats: 'paper'
+    }
+    
+]
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e=> {
+       const selectionName = selectionButton.dataset.selection
+       const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selection)
+    })
+})
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const compWinner = isWinner(computerSelection, selection)
+    if (yourWinner) increaseScore(yourScoreSpan)
+    if (compWinner) increaseScore(computerScoreSpan)
+
+    addSelectionResult(computerSelection, compWinner)
+    addSelectionResult(selection, yourWinner)
+}
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText=selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function increaseScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+
+}
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
